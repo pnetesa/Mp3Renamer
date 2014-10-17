daaad0538af1493da7082b051eaa9b849f85f244
Pavel Netesa <pavel.netesa@dataart.com>
2014-10-17 17:09:01 +0300

    Add: project files

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Mp3Renamer
+{
+	class Program
+	{
+		public const string Exe = ".exe";
+		public const string Wpl = ".wpl";
+		public const string Mp3 = ".mp3";
+		public const string Wma = ".wma";
+		public const string Wav = ".wav";
+
+		static void Main()
+		{
+			RenameFiles(AppDomain.CurrentDomain.BaseDirectory);
+		}
+
+		private static IEnumerable<string> GetFileNames(string basePath)
+		{
+			var playlistFiles = Directory.GetFiles(basePath, "*" + Wpl, SearchOption.TopDirectoryOnly);
+
+			if (playlistFiles.Length <= 0)
+			{
+				return Directory.GetFiles(basePath); // No playlist specified
+			}
+
+			var fileNames = new List<String>();
+			var doc = XDocument.Load(playlistFiles[0]);
+			var mediaElements = doc.Descendants("media");
+			foreach (var mediaElement in mediaElements)
+			{
+				var srcAttr = mediaElement.Attribute("src");
+				if (srcAttr == null)
+				{
+					continue;
+				}
+
+				var sepIndex = srcAttr.Value.LastIndexOf('\\');
+				var file = sepIndex >= 0 ? srcAttr.Value.Substring(sepIndex + 1) : srcAttr.Value;
+				fileNames.Add(Path.Combine(basePath, file));
+			}
+
+			return fileNames.ToArray();
+		}
+
+		private static void RenameFiles(string basePath)
+		{
+			var index = 0;
+
+			foreach (var fileName in GetFileNames(basePath))
+			{
+				if (!File.Exists(fileName))
+				{
+					continue;
+				}
+
+				if (fileName.EndsWith(Exe) || fileName.EndsWith(Wpl))
+				{
+					continue;
+				}
+
+				if (fileName.EndsWith(Mp3))
+				{
+					MoveToFile(basePath, fileName, index, Mp3);
+				}
+				else if (fileName.EndsWith(Wma))
+				{
+					MoveToFile(basePath, fileName, index, Wma);
+				}
+				else if (fileName.EndsWith(Wav))
+				{
+					MoveToFile(basePath, fileName, index, Wav);
+				}
+				else
+				{
+					MoveToFile(basePath, fileName, index, Mp3);
+				}
+
+				index++;
+			}
+
+			Console.WriteLine("Renamed {0} files.", index);
+		}
+
+		private static void MoveToFile(string basePath, string fileName, int index, string extenstion)
+		{
+			var destPath = GetPath(basePath, index, extenstion);
+			File.Move(fileName, destPath);
+			Console.WriteLine("{0} -> {1}", Path.GetFileName(fileName), Path.GetFileName(destPath));
+		}
+
+		private static string GetPath(string basePath, int index, string extenstion)
+		{
+			var destPath = Path.Combine(basePath, string.Format("{0:0000}{1}", index, extenstion));
+			var extraIndex = 0;
+			while (File.Exists(destPath))
+			{
+				destPath = Path.Combine(basePath, string.Format("{0:0000}_{2}{1}", index, extenstion, extraIndex++));
+			}
+			return destPath;
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mp3Renamer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pavel Netesa")]
+[assembly: AssemblyProduct("Mp3Renamer")]
+[assembly: AssemblyCopyright("Copyright © Pavel Netesa 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("232c23f1-dae2-41b5-8dfc-0355ce8cb2d9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
